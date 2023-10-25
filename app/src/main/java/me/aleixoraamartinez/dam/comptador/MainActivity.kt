6452e0306594d175e1f7b5d97d9e9bad1ba4af1f
Aleix Oraa <aoraa1@iesebre.com>
2023-10-25 17:27:29 +0200

    first

---
+++ app/src/main/java/me/aleixoraamartinez/dam/comptador/MainActivity.kt
@@ -0,0 +1,20 @@
+package me.aleixoraamartinez.dam.comptador
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.aleixoraamartinez.dam.comptador.ui.theme.ComptadorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_main)
+    } 
+}
